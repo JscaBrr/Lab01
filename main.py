5095139061c94c3cc1a2e7dd054153402ed4735b
JscaBrr <barroerojessica@yahoo.com>
2025-02-28 23:51:29 +0100

    versione 2

--- main.py
+++ main.py
@@ -1,102 +1,82 @@
 import random
+#1-dopo aver eseguito il fork e get from VCS
 #verde è nel repository locale ma non su repository github (ancora non sono stati collegati)
-#ho fatto delle modifiche nella mia working copy, voglio salvarle nel repository locale -> fare il commit -> si apre la finestra di commit -> file verdi sono nel repository e rossi no -> selezionare file da salvare e commentare
-
+#2-ho fatto delle modifiche nella mia working copy, voglio salvarle nel repository locale -> fare il commit -> si apre la finestra di commit -> file verdi sono nel repository e rossi no -> selezionare file da salvare e commentare > repository e working copy sono allineati sulla mia macchina
+#blu = file è nel repository locale ma anche nel repository remoto (è collegato il logate al sito github)
+#3-trasferire il repository locale sul repository che viene salvato sul sito web > push: stai prendendo il main e lo stai mandando nel main dell'origin == repository github
 class Domanda:
-    def __init__(self, testo, difficolta, rispostacorretta, risposteerrate):
+    def __init__(self, testo, livello, rispostacorretta, risposteerrate):
         self.testo = testo
-        self.difficolta = difficolta
+        self.livello = livello
         self.rispostacorretta = rispostacorretta
         self.risposteerrate = risposteerrate
-    def __str__(self):
-        return self.testo
 
-    def mescolarisposte(self):
-        risposte = [self.rispostacorretta]
-        risposte.extend(self.risposteerrate)
-        random.shuffle(risposte)
-        return risposte
-
-def caricadomande(file_path):
-    domande = []
+domande = []
+livelli = []
+def creadomande(file_path):
     with open (file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-
     i = 0
     while i < len(lines):
-        if lines[i].strip():
-            testo = lines[i].strip()
-            difficolta = int(lines[i+1].strip())
-            rispostacorretta = lines[i+2].strip()
-            risposteerrate = [lines[i+3].strip(), lines[i+4].strip(), lines[i+5].strip()]
-            domanda = Domanda(testo, difficolta, rispostacorretta, risposteerrate)
-            domande.append(domanda)
-            i+=6
+        t = lines[i].strip()
+        l = lines[i+1].strip()
+        livelli.append(l)
+        rc = lines[i+2].strip()
+        re = [lines[i+3].strip(), lines[i+4].strip(), lines[i+5].strip()]
+        domanda = Domanda(t,l,rc,re)
+        domande.append(domanda)
+        i += 7
+        if i == len(lines)-1:
+            break
+
+creadomande("domande.txt")
+duplicadomande = domande.copy()
+random.shuffle(duplicadomande)
+
+class Persona:
+    def __init__(self, nickname, punteggio):
+        self.nickname = nickname
+        self.punteggio = punteggio
+
+i = int(min(livelli))
+punteggio = 0
+while len(duplicadomande) > 0:
+    d = random.choice(duplicadomande)
+    if int(d.livello) == i:
+        print(f"{d.testo}\n" )
+        risposte = d.risposteerrate.copy()
+        risposte.extend([d.rispostacorretta])
+        random.shuffle(risposte)
+        for j in risposte:
+            print(f"{risposte.index(j)+1}) {j}")
+        answer = input("\nInserisci la risposta corretta:")
+        if int(answer)-1 == risposte.index(d.rispostacorretta):
+            print("Risposta esatta !\n")
+            punteggio += 1
+            i += 1
+            duplicadomande.remove(d)
+            if i == int(max(livelli)):
+                print("Hai raggiunto il livello massimo")
+                break
         else:
-            i+=1
-    return domande
+            print("Risposta errata !")
+            break
+    else:
+        continue
+n = input("Inserisci il nickame:")
+p = Persona(n, punteggio)
+try:
+    with open("punti.txt", "a", encoding='utf-8') as file:
+        file.write(f"{p.nickname} {p.punteggio}\n")
 
-class Gioco:
-    def __init__(self, domande):
-        self.domande = domande
-        self.punteggio = 0
-        self.livello = 0
+    with open("punti.txt", "r", encoding='utf-8') as file:
+        linee = file.readlines()
+        linee = sorted(linee, key = lambda x: int(x.split()[1]), reverse = True)
 
-    def gioca(self):
-        domandecopia = self.domande
-        levels = []
-        for domanda in domandecopia:
-            levels.append(domanda.difficolta)
-
-        for domanda in domandecopia:
-            if domanda.difficolta == self.livello:
-                domandecopia.remove(domanda)
-                print(f"\nLivello {self.livello}) {domanda.testo}")
-                risposte = domanda.mescolarisposte()
-
-                for i, j in enumerate(risposte, 1):
-                    print(f"\t{i}. {j}")
-
-                rispostautente = input("Inserisci la risposta: ")
-                if risposte[int(rispostautente) - 1] == domanda.rispostacorretta:
-                    print("Risposta corretta!")
-                    self.punteggio += 1
-                    self.livello += 1
-                    if self.livello == max(levels)+1:
-                        print("Hai raggiunto il livello massimo!")
-                        break
-                else:
-                    print(f"Risposta sbagliata! La risposta corretta era: {domanda.rispostacorretta}")
-                    break
-
-
-
-
-def salvapunteggio(nickname, punteggio):
-    try:
-        with open("punti.txt", "a", encoding = 'utf-8') as file:
-            file.write(f"{nickname} {punteggio}\n")
-
-        with open("punti.txt", "r", encoding='utf-8') as file:
-            linee = file.readlines()
-            linee = sorted(linee, key=lambda x: int(x.split()[1]), reverse = True)
-
-        with open("punti.txt", "w", encoding='utf-8') as file:
-            file.writelines(linee)
-
-    except Exception as e:
-        print(f"Errore nel salvataggio dei punteggi: {e}")
-
-def main():
-    domande = caricadomande("domande.txt")
-    gioco = Gioco(domande)
-    gioco.gioca()
-    nickname = input("Inserisci il tuo nickname:")
-    salvapunteggio(nickname, gioco.punteggio)
-
-if __name__ == "__main__":
-    main()
-
+    with open("punti.txt", "w", encoding='utf-8') as file:
+        file.writelines(linee)
+except Exception as e:
+    print(f"Errore nel salvataggio dei punteggi: {e}")
 
 
 
